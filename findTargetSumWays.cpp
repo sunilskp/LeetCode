736f9df49afd713d4e817843c8727ead0bc323ae
Sunil Kumar Palariya <44226373+sunilskp@users.noreply.github.com>
2020-08-04 22:18:51 +0530

    Update findTargetSumWays.cpp

--- findTargetSumWays.cpp
+++ findTargetSumWays.cpp
@@ -23,3 +23,39 @@
         }    
         
     }
+// using Dynamic Programming
+
+class Solution {
+   
+public:
+    int findTargetSumWays(vector<int>& nums, int S)
+    {
+             int len = nums.size();
+             int sum = 0;  
+             for (int i = 0; i < len; i++) 
+             sum += nums[i]; 
+             if(sum<S || -S < -sum)
+             {
+                 return 0;
+             }
+             if((sum+S)%2==1)
+             {
+                 return 0;
+             }
+             int diff = (sum+S)/2;
+             int dp [diff+1];
+             memset(dp,0,sizeof(dp));     
+             dp[0]=1;
+            for (int i=0; i<len; i++) 
+            { 
+            
+                for (int j=diff; j>=nums[i]; j--) 
+                { 
+                    dp[j] = dp[j]+dp[j-nums[i]]; 
+                } 
+
+            } 
+        return dp[diff];
+        
+    }
+};
