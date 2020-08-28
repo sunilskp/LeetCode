ada9a8c8c1155323c577978e1dec49425bcb2d67
Sunil Kumar Palariya <44226373+sunilskp@users.noreply.github.com>
2020-08-28 22:00:52 +0530

    Update countPrimes.cpp

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        if(n<2) return 0;
         bool prime[n+1];
-        for (int i = 2; i < n + 1; i ++) {
-            prime[i] = true;
-        }
-        for (int i = 2; i * i < n; i ++) {
-            if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
+        memset(prime,true,sizeof(prime));
+        prime[0] = false;
+        prime[1] = false;
+        for (int i = 2; i * i < n; i ++)
+        {
+            if (prime[i]) 
+            {
+                for (int j = i * i; j < n; j += i)
+                {
                     prime[j] = false;
                 }
             }
         }
         int cnt = 0;
-        for (int i = 2; i < n; i ++) {
-            if (prime[i]) {
+        for (int i = 0; i < n; i ++)
+        {
+            if (prime[i])
+            {
                 cnt ++;
             }
         }
