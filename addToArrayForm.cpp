1f30449a737f59ed14ab10e0d2aa0f52a8477b05
Sunil Kumar Palariya <44226373+sunilskp@users.noreply.github.com>
2020-09-13 09:30:15 +0530

    Create addToArrayForm.cpp

---
+++ addToArrayForm.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& A, int K) {
+        int len = A.size();
+        vector<int>res;
+        int c =0,i = len-1;
+        while( i>=0 || K!=0 ) 
+        {
+           if(i>=0 && K!=0 )
+            { 
+                int s = (A[i]) + (K%10) + c;
+                c = s/10;
+                res.push_back(s%10);
+                K = K/10;
+                i--;
+            }
+            else if(i>=0 && K==0  )
+            {
+                int s = (A[i]) + c;
+                c = s/10;
+                res.push_back(s%10);
+                i--;
+            }
+            else if(K!=0 && i == -1)
+            {
+                int s = (K%10)+c;
+                c = s/10;
+                res.push_back(s%10);
+                K = K/10;
+            }
+        }
+        if(c)
+        {
+            res.push_back(c);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
